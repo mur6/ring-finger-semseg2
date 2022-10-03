2ac4d576b4bbff1a7f17b1eede61b66cb5dec57b
MURAKI Taichi <muraki.taichi@gmail.com>
2022-10-03 17:01:24 +0900

    fix

--- train.py
+++ train.py
@@ -85,48 +85,10 @@ for epoch in range(1, 1 + 1):
         # evaluate
         points = ((points - (224 / 2.0)) / 112.0).float()
 
-        # pred_labels = predicted[mask].detach().cpu().numpy()
-        # true_labels = labels[mask].detach().cpu().numpy()
-        # accuracy = accuracy_score(pred_labels, true_labels)
-        # accuracies.append(accuracy)
-        # pbar.set_postfix(
-        #     {"Batch": idx, "Pixel-wise accuracy": sum(accuracies) / len(accuracies), "Loss": sum(losses) / len(losses)}
-        # )
-
-        # backward + optimize
         loss = criterion(outputs, points)
         train_loss += loss.item()
         loss.backward()
         optimizer.step()
-        # pixel_values = batch[0].to(device)
-        # # maskes = batch[1].to(device)
-        # points = batch[2].to(device)
-
-        # # zero the parameter gradients
-        # optimizer.zero_grad()
-
-        # # forward
-        # outputs = model(pixel_values=pixel_values)
-
-        # # evaluate
-        # upsampled_logits = nn.functional.interpolate(
-        #     outputs.logits, size=labels.shape[-2:], mode="bilinear", align_corners=False
-        # )
-        # predicted = upsampled_logits.argmax(dim=1)
-
-        # pred_labels = predicted[mask].detach().cpu().numpy()
-        # true_labels = labels[mask].detach().cpu().numpy()
-        # accuracy = accuracy_score(pred_labels, true_labels)
-        # loss = outputs.loss
-        # accuracies.append(accuracy)
-        # losses.append(loss.item())
-        # pbar.set_postfix(
-        #     {"Batch": idx, "Pixel-wise accuracy": sum(accuracies) / len(accuracies), "Loss": sum(losses) / len(losses)}
-        # )
-
-        # # backward + optimize
-        # loss.backward()
-        # optimizer.step()
     else:
         model.eval()
         val_loss = 0.0
@@ -138,34 +100,7 @@ for epoch in range(1, 1 + 1):
                 outputs = model(pixel_values=pixel_values)
                 loss = criterion(outputs, points)
                 val_loss += loss.item()
-        # with torch.no_grad():
-        #     for idx, batch in enumerate(valid_dataloader):
-        #         pixel_values = batch["pixel_values"].to(device)
-        #         labels = batch["labels"].to(device)
 
-        #         outputs = model(pixel_values=pixel_values, labels=labels)
-        #         upsampled_logits = nn.functional.interpolate(
-        #             outputs.logits, size=labels.shape[-2:], mode="bilinear", align_corners=False
-        #         )
-        #         predicted = upsampled_logits.argmax(dim=1)
-
-        #         mask = labels != 0  # we don't include the background class in the accuracy calculation
-        #         pred_labels = predicted[mask].detach().cpu().numpy()
-        #         true_labels = labels[mask].detach().cpu().numpy()
-        #         accuracy = accuracy_score(pred_labels, true_labels)
-        #         val_loss = outputs.loss
-        #         val_accuracies.append(accuracy)
-        #         val_losses.append(val_loss.item())
-    # writer.add_scalar('Loss/train', sum(losses)/len(losses), epoch)
-    # writer.add_scalar('Loss/val', sum(val_losses)/len(val_losses), epoch)
-    # writer.add_scalar('Accuracy/train', sum(accuracies)/len(accuracies), epoch)
-    # writer.add_scalar('Accuracy/val', sum(val_accuracies)/len(val_accuracies), epoch)
-    # print(
-    #     f"Train Pixel-wise accuracy: {sum(accuracies)/len(accuracies)}\
-    #      Train Loss: {sum(losses)/len(losses)}\
-    #      Val Pixel-wise accuracy: {sum(val_accuracies)/len(val_accuracies)}\
-    #      Val Loss: {sum(val_losses)/len(val_losses)}"
-    # )
     train_count = len(train_dataloader)
     val_count = len(valid_dataloader)
     s1 = f"Training: Mean Squared Error: {train_loss/train_count}"
