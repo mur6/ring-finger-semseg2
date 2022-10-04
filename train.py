2d0d0b0fe9b046d108e35ef795fc6b10aa453ecd
MURAKI Taichi <muraki.taichi@gmail.com>
2022-10-04 17:09:21 +0900

    fix

--- train.py
+++ train.py
@@ -4,36 +4,34 @@ from pathlib import Path
 from typing import Optional, Tuple, Union
 
 import albumentations as A
-import cv2
+import hydra
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from albumentations.pytorch import ToTensorV2
+from omegaconf import DictConfig, OmegaConf
 from PIL import Image
 from skimage import measure
+from sklearn.metrics import accuracy_score
 from torch import nn
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from torchvision import transforms as transforms
 from torchvision.transforms import Resize, ToTensor
-from transformers import SegformerDecodeHead, SegformerFeatureExtractor, SegformerForSemanticSegmentation
+from tqdm import tqdm
+from transformers import AdamW, SegformerDecodeHead, SegformerFeatureExtractor, SegformerForSemanticSegmentation
 from transformers.models.segformer.modeling_segformer import SegformerMLP, SegformerModel
 
 from src.dataset import RingFingerDataset
+from src.loader import make_dataloaders
 from src.model import get_model
 
-#base_data_dir = Path("../blender-for-finger-segmentation/data2/")
-
-from torch import nn
-from torch.utils.data import DataLoader
-import torch
-from sklearn.metrics import accuracy_score
-from tqdm import tqdm
-from transformers import AdamW
+# base_data_dir = Path("../blender-for-finger-segmentation/data2/")
+# pbar = tqdm(train_dataloader)
 
 
-def prepare_model():
+def train(*, max_epoch, train_dataloader, valid_dataloader):
     model = get_model()
     criterion = nn.MSELoss()
     optimizer = AdamW(model.parameters(), lr=0.00006)
@@ -41,10 +39,7 @@ def prepare_model():
     model.to(device)
     print("Model Initialized!")
 
-# pbar = tqdm(train_dataloader)
-
-def train():
-    for epoch in range(1, 1 + 1):
+    for epoch in range(1, max_epoch + 1):
         print("Epoch:", epoch)
         pbar = tqdm(train_dataloader)
         accuracies = []
@@ -87,3 +82,17 @@ def train():
         s1 = f"Training: Mean Squared Error: {train_loss/train_count}"
         s2 = f"Validation: Mean Squared Error: {val_loss/val_count}"
         print(s1 + " " + s2)
+
+
+@hydra.main(version_base=None, config_name="config/local")
+def main(cfg):
+    max_epoch = int(cfg.config.max_epoch)
+    base_data_dir = Path(cfg.config.base_data_dir)
+    train_dataset, valid_dataset, train_dataloader, valid_dataloader = make_dataloaders(base_data_dir)
+    train(
+        max_epoch=max_epoch,
+    )
+
+
+if __name__ == "__main__":
+    main()
